e9eb50eb0ecefb66a2e5b88fd3ec5f2f55146fd9
hegaoye@qq.com <hegaoye@qq.com>
2019-06-10 12:59:42 +0100

    add file basesv.py

---
+++ diglett/service/basesv.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+import configparser
+
+from diglett import app
+
+
+class BaseSV:
+    def __init__(self):
+        cf = configparser.ConfigParser()
+        cf.read(app.config['PITOP_CONF'])
+        host = cf.get("sys", "host")
+        nat_check_url = cf.get("sys", "nat_check_url")
+        self.local_port = cf.get("sys", "local_port")
+        self.natConfigFile = cf.get("sys", "nat_config_file")
+        self.group_code = cf.get("sys", "group_code")
+        self.host = host
+        # 获取电池信息指令
+        self.checkBatteryCommand = 'pt-battery'
+        # 注册设备
+        self.regUri = self.host + '/devicePool/build.shtml'
+        # 通知上线成功
+        self.notifyUri = self.host + '/devicePool/notify.shtml?code={code}&domain={domain}'
+        # 检测穿透接口
+        self.pingUri = nat_check_url
