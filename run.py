e62a2c0f62e5ce064ad04817964e46d6cb570a79
hegaoye <hegaoye@qq.com>
2019-06-21 10:01:20 +0800

    增加兼容模式

--- run.py
+++ run.py
@@ -12,14 +12,14 @@ if __name__ == "__main__":
     logging.config.fileConfig('logging.conf')
     log = logging.getLogger(__name__)
     log.info('>>>>> Starting server <<<<<')
-    # try:
-    # 启动flask 服务
-    cf = configparser.ConfigParser()
-    cf.read(app.config['PITOP_CONF'], encoding='UTF-8')
-    local_port = cf.get("sys", "local_port")
-    RegNat(local_port).start()
-    log.info("The Server  port [" + local_port + "]")
-    server = pywsgi.WSGIServer(('0.0.0.0', int(local_port)), app, log=log, handler_class=WebSocketHandler)
-    server.serve_forever()
-    # except Exception as e:
-    #     log.error("server starting failed")
+    try:
+        # 启动flask 服务
+        cf = configparser.ConfigParser()
+        cf.read(app.config['PITOP_CONF'], encoding='UTF-8')
+        local_port = cf.get("sys", "local_port")
+        RegNat(local_port).start()
+        log.info("The Server  port [" + local_port + "]")
+        server = pywsgi.WSGIServer(('0.0.0.0', int(local_port)), app, log=log, handler_class=WebSocketHandler)
+        server.serve_forever()
+    except Exception as e:
+        log.error("server starting failed")
