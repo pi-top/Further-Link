4c98ad88c39e7565ce698f45a4699c357fe8bf1c
angusjfw <angusjfw@gmail.com>
2019-10-30 16:00:26 +0000

    Delete almost everything

--- run.py
+++ run.py
@@ -1,26 +1,8 @@
-# coding=utf-8
-import configparser
-import logging.config
-
-from reg_thread import RegToServer
-from settings import LOGGING_CONF
 from src import app
 
 if __name__ == "__main__":
-    from gevent import pywsgi
-    from geventwebsocket.handler import WebSocketHandler
+  from gevent import pywsgi
+  from geventwebsocket.handler import WebSocketHandler
 
-    logging.config.fileConfig(LOGGING_CONF)
-    log = logging.getLogger(__name__)
-    log.info('>>>>> Starting server <<<<<')
-    try:
-        # start flask service
-        cf = configparser.ConfigParser()
-        cf.read(app.config['PITOP_CONF'], encoding='UTF-8')
-        local_port = cf.get("sys", "local_port")
-        RegToServer(local_port).start()
-        log.info("The Server  port [" + local_port + "]")
-        server = pywsgi.WSGIServer(('0.0.0.0', int(local_port)), app, log=log, handler_class=WebSocketHandler)
-        server.serve_forever()
-    except Exception as e:
-        log.error("server starting failed")
+  server = pywsgi.WSGIServer(('127.0.0.1', 8080), app, handler_class=WebSocketHandler)
+  server.serve_forever()
--- src/__init__.py
+++ src/__init__.py
@@ -1,35 +1,13 @@
-# coding=utf-8
 from flask import Flask
-from flask_cors import CORS
 from flask_sockets import Sockets
 
-from src.base.log4py import logger
-from src.base.tools.regexConverter import RegexConverter
-from src.rest import api, api_blueprint
-from src.rest.endpoints import execute
-from src.rest.endpoints.battery import ns_battery
-from src.rest.endpoints.beatheart import ns_beatheart
-from src.rest.endpoints.codefile import ns_file
-from src.rest.endpoints.execute import ns_exec
+# import execute
 
 app = Flask(__name__)
-sockets = Sockets(app)
-# 跨域设置，允许跨域
-CORS(app, supports_credentials=True, resources={r'/*': {"origins": "*"}})
 
-app.config.from_object('settings')
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-app.url_map.converters['regex'] = RegexConverter
+@app.route('/status')
+def ok():
+    return 'OK'
 
-logger.info("配置文件路径 ====> " + app.config["PITOP_CONF"])
-logger.info("缓存文件路径 ====> " + app.config["CACHEDATA_JSON"])
-
-api.add_namespace(ns_battery)
-api.add_namespace(ns_beatheart)
-api.add_namespace(ns_exec)
-# api.add_namespace(ns_file)
-
-app.register_blueprint(blueprint=api_blueprint)
-
-# ws
-sockets.register_blueprint(execute.ws)
+# sockets = Sockets(app)
+# sockets.register_blueprint(execute.ws)
