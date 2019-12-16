830821683275ab0675a1b893ad1dbb9e818e4b7b
angusjfw <angusjfw@gmail.com>
2019-12-16 13:46:47 +0000

    Add cert common name

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 openssl req -x509 -newkey rsa:4096 -nodes -days 365 \
   -out "$DIR"/cert.pem -keyout "$DIR"/key.pem \
-  -subj "/C=GB/ST=London/O=CEED Ltd/emailAddress=suport@pi-top.com"
+  -subj "/C=GB/ST=London/O=CEED Ltd/CN=pi-top/emailAddress=suport@pi-top.com"
 
 /usr/bin/python3 -B "$DIR"/pt-further-link-server.py
