ce7bb567d1076b053a195b6675dd506cf6c4d30b
angusjfw <angusjfw@gmail.com>
2019-12-18 16:15:57 +0000

    Add timeout to ipc send method

--- src/lib/further_link.py
+++ src/lib/further_link.py
@@ -14,6 +14,7 @@ try:
         ipc_filename = __main__.__file__.replace('.py', '.' + name + '.sock')
         ipc_channels[name] = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         ipc_channels[name].connect(ipc_filename)
+        ipc_channels[name].settimeout(0.1)
 except:
     print('Warning: Module further_link cannot be used in this context')
 
