9dc8683cd0a79a105dabd1ae57bd3c9a6ee20ae0
angusjfw <angusjfw@gmail.com>
2020-03-06 12:52:16 +0000

    Fix ipc system

--- src/process_handler.py
+++ src/process_handler.py
@@ -30,21 +30,6 @@ class ProcessHandler:
         if self.is_running() or not isinstance(script, str):
             raise InvalidOperation()
 
-        self.ipc_channels = {}
-        for name in IPC_CHANNELS:
-            ipc_filename = self._get_ipc_filename(name)
-            # self.ipc_channels[name] = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            # self.ipc_channels[name].bind(ipc_filename)
-            # self.ipc_channels[name].setblocking(0)
-            # self.ipc_channels[name].listen(1)
-            handle_ipc = partial(self.handle_ipc, channel=name)
-            await asyncio.start_unix_server(handle_ipc, path=)
-
-
-https: // docs.python.org/3/library/asyncio-stream.html  # asyncio.start_unix_server
-https: // github.com/pi-top/pt-further-link-deb/blob/master/src/process_handler.py
-https: // stackoverflow.com/questions/48506460/python-simple-socket-client-server-using-asyncio
-
         main_filename = self._get_main_filename()
         async with aiofiles.open(main_filename, 'w+') as file:
             await file.write(script)
@@ -83,13 +68,17 @@ https: // stackoverflow.com/questions/48506460/python-simple-socket-client-serve
         return self.work_dir + '/' + self.id + '.' + channel + '.sock'
 
     async def _communicate(self):
-        for name in self.ipc_channels:
-            asyncio.create_task(self._handle_ipc(name)),
-
-        await asyncio.wait([
+        io_tasks = [
             asyncio.create_task(self._handle_output('stdout')),
             asyncio.create_task(self._handle_output('stderr'))
-        ])
+        ]
+
+        for name in IPC_CHANNELS:
+            io_tasks.append(asyncio.create_task(
+                self._handle_ipc(name)
+            ))
+
+        await asyncio.wait(io_tasks)
 
         exit_code = await self.process.wait()
         self.process = None
@@ -109,6 +98,30 @@ https: // stackoverflow.com/questions/48506460/python-simple-socket-client-serve
             else:
                 break
 
+    async def _handle_ipc(self, channel):
+        async def handle_connection(reader, _):
+            message = ''
+            while True:
+                if not self.is_running():
+                    break
+
+                data = await reader.read(4096)
+                if data == b'':
+                    break
+
+                tokens = data.decode("utf-8").strip().split()
+                if tokens[0] == channel:
+                    if len(message) > 0:
+                        if self.on_output:
+                            await self.on_output(channel, message)
+                        message = ''
+                    message += tokens[1]
+                else:
+                    message += tokens[0]
+
+        ipc_filename = self._get_ipc_filename(channel)
+        await asyncio.start_unix_server(handle_connection, path=ipc_filename)
+
     async def _clean_up(self):
         try:
             await aiofiles.os.remove(self._get_main_filename())
@@ -119,49 +132,3 @@ https: // stackoverflow.com/questions/48506460/python-simple-socket-client-serve
                     pass
         except:
             pass
-
-    async def _handle_ipc(self, channel):
-        # listen
-        # accept
-        # recv
-        stream = getattr(self.process, stream_name)
-        while True:
-            line = await stream.readline()
-            output = line.decode(encoding='utf-8')
-            if line:
-                if self.on_output:
-                    await self.on_output(stream_name, output)
-            else:
-                break
-
-    # def handle_ipc(self, channel):
-    #     self.ipc_channels[channel].listen(1)
-    #     while True:
-    #         if not self.is_running():
-    #             break
-    #         try:
-    #             conn, addr = self.ipc_channels[channel].accept()
-    #             message = ''
-    #             while True:
-    #                 if not self.is_running():
-    #                     break
-    #                 try:
-    #                     data = conn.recv(4096)
-    #                     if data:
-    #                         tokens = data.decode("utf-8").strip().split()
-    #                         if tokens[0] == channel:
-    #                             if len(message) > 0:
-    #                                 self.websocket.send(create_message(channel, {
-    #                                     'message': message
-    #                                 }))
-    #                                 message = ''
-    #                             message += tokens[1]
-    #                         else:
-    #                             message += tokens[0]
-    #                 except:
-    #                     sleep(0.001)
-    #                     continue
-    #             conn.close()
-    #         except:
-    #             sleep(0.1)
-    #             continue
