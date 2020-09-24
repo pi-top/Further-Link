8113f18c8b333690a4a58ee9edeea11d8a3c6949
Angus Whitehead <angusjfw@gmail.com>
2020-09-24 17:21:09 +0100

    Improve test upload data

--- tests/test_data/upload_data.py
+++ tests/test_data/upload_data.py
@@ -1,28 +1,28 @@
 directory = {
     "name": "5f3542eec089508fd258e631",
     "files": {
-        "ultrasonic2.svg": {
+        "cereal.csv": {
             "type": "url",
             "content": {
-                "url": "http://localhost:8001/api/v3/Containers/further-files/download/fb08c5e2-e561-42d1-b89e-a78625a118d9.svg",
+                "url": "https://backend.pi-top.com/api/v3/Containers/further-files/download/2a9a1e12-b71c-4f20-84b2-f205e9dc16fd.csv",
                 "bucketName": "further-files",
-                "fileName": "fb08c5e2-e561-42d1-b89e-a78625a118d9.svg"
+                "fileName": "2a9a1e12-b71c-4f20-84b2-f205e9dc16fd.csv"
             }
         },
         "cockerhampriory.jpeg": {
             "type": "url",
             "content": {
-                "url": "http://localhost:8001/api/v3/Containers/further-files/download/ea3cb61e-7f31-4c52-8735-2cefe83302ee.jpeg",
+                "url": "https://backend.pi-top.com/api/v3/Containers/further-files/download/ea3cb61e-7f31-4c52-8735-2cefe83302ee.jpeg",
                 "bucketName": "further-files",
                 "fileName": "ea3cb61e-7f31-4c52-8735-2cefe83302ee.jpeg"
             }
         },
-        "ploverscar.jpeg": {
+        "kitty.jpeg": {
             "type": "url",
             "content": {
                 "url": "https://placekitten.com/50/50",
                 "bucketName": "further-files",
-                "fileName": "71352c27-8e36-4372-a682-9d95be20bb96.jpeg"
+                "fileName": "81352c27-8e36-4372-b682-9d95be20bb97.jpeg"
             }
         }
     }
