b2e12948bc83690baf617f751eb7cb411b283f36
Angus Whitehead <angusjfw@gmail.com>
2020-11-19 18:13:23 +0000

    Fix further_link lib ipc path

--- src/lib/further_link/__init__.py
+++ src/lib/further_link/__init__.py
@@ -24,7 +24,7 @@ try:
         ipc_filename = main_filename.replace('.py', '.' + name + '.sock')
         ipc_path = os.path.join(get_temp_dir(), ipc_filename)
         ipc_channels[name] = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        ipc_channels[name].connect(ipc_filename)
+        ipc_channels[name].connect(ipc_path)
         ipc_channels[name].settimeout(0.1)
 except Exception:
     print('Warning: Module further_link cannot be used in this context')
