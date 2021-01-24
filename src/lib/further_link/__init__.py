84781fb455c479dd3137fd848e14e46e1b6e3d90
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-01-24 16:04:49 +0000

    Add PIL to OpenCV conversion support

--- src/lib/further_link/__init__.py
+++ src/lib/further_link/__init__.py
@@ -1,41 +1,50 @@
 # This package is made available to user code running with further-link
 
-import os
-import socket
-from base64 import b64encode
-
-import __main__
+from pitop.camera.pil_opencv_conversion import pil_to_opencv
 
 # __version__ made available for users
 from .version import __version__
 
+from os import environ, path
+from socket import (
+    socket,
+    AF_UNIX,
+    SOCK_STREAM,
+)
+from base64 import b64encode
+
+import __main__
+
 ipc_channel_names = ['video']
 ipc_channels = {}
 
-main_filename = os.path.basename(__main__.__file__)
+main_filename = path.basename(__main__.__file__)
 
 
 def get_temp_dir():
-    return os.environ.get('FURTHER_LINK_TEMP_DIR', '/tmp')
+    return environ.get('FURTHER_LINK_TEMP_DIR', '/tmp')
 
 
 try:
     for name in ipc_channel_names:
         ipc_filename = main_filename.replace('.py', '.' + name + '.sock')
-        ipc_path = os.path.join(get_temp_dir(), ipc_filename)
-        ipc_channels[name] = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        ipc_path = path.join(get_temp_dir(), ipc_filename)
+        ipc_channels[name] = socket(AF_UNIX, SOCK_STREAM)
         ipc_channels[name].connect(ipc_path)
         ipc_channels[name].settimeout(0.1)
 except Exception:
     print('Warning: Module further_link cannot be used in this context')
 
 
-def send_image(frame):
+def send_image(frame, format="PIL"):
     try:
         from cv2 import imencode
     except ImportError as e:
         print(e)
         return
+
+    if format == "PIL":
+        frame = pil_to_opencv(frame)
 
     try:
         _, buffer = imencode('.jpg', frame)
