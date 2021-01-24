039f11e963ad6eff058ee512c9f80e95baa75221
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-01-24 16:51:15 +0000

    Undo unnecessary changes

--- src/lib/further_link/__init__.py
+++ src/lib/further_link/__init__.py
@@ -1,35 +1,32 @@
 # This package is made available to user code running with further-link
 
+import numpy as np
+from PIL import Image
+
+import os
+import socket
+from base64 import b64encode
+
 import __main__
 
 # __version__ made available for users
 from .version import __version__
 
-from base64 import b64encode
-import numpy as np
-from os import environ, path
-from PIL import Image
-from socket import (
-    socket,
-    AF_UNIX,
-    SOCK_STREAM,
-)
-
 ipc_channel_names = ['video']
 ipc_channels = {}
 
-main_filename = path.basename(__main__.__file__)
+main_filename = os.path.basename(__main__.__file__)
 
 
 def get_temp_dir():
-    return environ.get('FURTHER_LINK_TEMP_DIR', '/tmp')
+    return os.environ.get('FURTHER_LINK_TEMP_DIR', '/tmp')
 
 
 try:
     for name in ipc_channel_names:
         ipc_filename = main_filename.replace('.py', '.' + name + '.sock')
-        ipc_path = path.join(get_temp_dir(), ipc_filename)
-        ipc_channels[name] = socket(AF_UNIX, SOCK_STREAM)
+        ipc_path = os.path.join(get_temp_dir(), ipc_filename)
+        ipc_channels[name] = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
         ipc_channels[name].connect(ipc_path)
         ipc_channels[name].settimeout(0.1)
 except Exception:
