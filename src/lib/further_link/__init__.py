121dc06bfb280bc538fa1b03405400019969a16e
Jorge Capona <jcapona@gmail.com>
2021-02-02 10:46:42 -0300

    Use SDK image conversion functions on send_image

--- src/lib/further_link/__init__.py
+++ src/lib/further_link/__init__.py
@@ -12,6 +12,8 @@ try:
 except ImportError as e:
     print(e)
 
+from pitop.core import ImageFunctions
+
 # __version__ made available for users
 from .version import __version__
 
@@ -37,44 +39,12 @@ except Exception:
     print('Warning: Module further_link cannot be used in this context')
 
 
-# Taken from SDK, to avoid hard dependency
-def _image_has_3_channels(image):
-    return len(image.shape) == 3
-
-
-def _image_rgb_bgr_convert(image):
-    return image[:, :, ::-1]
-
-
-def _pil_to_opencv(image):
-    from numpy import array
-    image_arr = array(image)
-
-    if _image_has_3_channels(image_arr):
-        # Array has 3 channel, do nothing
-        image = _image_rgb_bgr_convert(image_arr)
-    else:
-        image = image_arr
-
-    return image
-
-
-def _opencv_to_pil(image):
-    if len(image.shape) == 3:
-        image = Image.fromarray(_image_rgb_bgr_convert(image))
-    else:
-        # Not 3 channel, just convert
-        image = Image.fromarray(image)
-
-    return image
-
-
 def send_image(frame, format=None):
     if format is not None:
         print("The 'format' parameter is no longer required in this function. Both PIL and OpenCV formats can be used without specifying which.")
 
     if isinstance(frame, Image.Image):
-        frame = _pil_to_opencv(frame)
+        frame = ImageFunctions.convert(frame, format="opencv")
 
     try:
         _, buffer = imencode('.jpg', frame)
