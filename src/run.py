8fa01e8ee2b99a1815afceb2f913577450b65296
Angus Whitehead <angusjfw@gmail.com>
2021-05-27 11:40:11 +0100

    Refactor run endpoint

--- src/run.py
+++ src/run.py
@@ -24,7 +24,55 @@ class RunManager:
 
     def stop(self):
         for p in self.process_handlers.values():
-            p.stop()
+            try:
+                p.stop()
+            except InvalidOperation:
+                pass
+
+    async def handle_message(self, message):
+        try:
+            m_type, m_data, m_process = parse_message(message)
+
+            process_handler = self.process_handlers.get(m_process)
+
+            if m_type == 'ping':
+                await self.socket.send_str(create_message('pong'))
+
+            elif m_type == 'start' and not process_handler and m_process:
+                await self.add_handler(m_process, m_data.get('runner'), m_data)
+
+            elif (m_type == 'stdin'
+                  and process_handler
+                  and 'input' in m_data
+                  and isinstance(m_data.get('input'), str)):
+                await process_handler.send_input(m_data['input'])
+
+            elif m_type == 'stop' and process_handler:
+                process_handler.stop()
+
+            elif (m_type == 'keyevent'
+                  and process_handler
+                  and 'key' in m_data
+                  and isinstance(m_data.get('key'), str)
+                  and 'event' in m_data
+                  and isinstance(m_data.get('event'), str)):
+                await process_handler.send_key_event(m_data['key'],
+                                                     m_data['event'])
+
+            else:
+                raise BadMessage()
+
+        except (BadMessage, InvalidOperation):
+            logging.exception(f'{self.id} Bad Message')
+            await self.socket.send_str(
+                create_message('error', {'message': 'Bad message'})
+            )
+
+        except Exception:
+            logging.exception(f'{self.id} Message Exception')
+            await self.socket.send_str(
+                create_message('error', {'message': 'Message Exception'})
+            )
 
     async def add_handler(self, process_id, runner, m_data):
         handler = None
@@ -87,43 +135,6 @@ class RunManager:
         self.process_handlers[id] = handler
 
 
-async def handle_message(message, run_manager, socket):
-    m_type, m_data, m_process = parse_message(message)
-
-    process_handler = run_manager.process_handlers.get(m_process)
-
-    print(run_manager.process_handlers)
-    print(m_process)
-    print(process_handler)
-    print(not process_handler)
-
-    if m_type == 'ping':
-        await socket.send_str(create_message('pong'))
-
-    elif m_type == 'start' and not process_handler and m_process:
-        await run_manager.add_handler(m_process, m_data.get('runner'), m_data)
-
-    elif (m_type == 'stdin'
-          and process_handler
-          and 'input' in m_data
-          and isinstance(m_data.get('input'), str)):
-        await process_handler.send_input(m_data['input'])
-
-    elif m_type == 'stop' and process_handler:
-        process_handler.stop()
-
-    elif (m_type == 'keyevent'
-          and process_handler
-          and 'key' in m_data
-          and isinstance(m_data.get('key'), str)
-          and 'event' in m_data
-          and isinstance(m_data.get('event'), str)):
-        await process_handler.send_key_event(m_data['key'], m_data['event'])
-
-    else:
-        raise BadMessage()
-
-
 async def run(request):
     query_params = request.query
     user = query_params.get('user', None)
@@ -137,30 +148,13 @@ async def run(request):
 
     try:
         async for message in socket:
-            logging.debug(
-                f'{run_manager.id} Received Message {message.data}'
-            )
-            try:
-                await handle_message(message.data, run_manager, socket)
-
-            except (BadMessage, InvalidOperation):
-                logging.exception(f'{run_manager.id} Bad Message')
-                await socket.send_str(
-                    create_message('error', {'message': 'Bad message'})
-                )
-
-            except Exception:
-                logging.exception(f'{run_manager.id} Message Exception')
-                await socket.send_str(
-                    create_message('error', {'message': 'Message Exception'})
-                )
+            logging.debug(f'{run_manager.id} Received Message {message.data}')
+            await run_manager.handle_message(message.data)
 
     except asyncio.CancelledError:
         pass
+
     finally:
         await socket.close()
-
-    logging.info(f'{run_manager.id} Closed connection')
-    run_manager.stop()
-
-    return socket
+        logging.info(f'{run_manager.id} Closed connection')
+        run_manager.stop()
