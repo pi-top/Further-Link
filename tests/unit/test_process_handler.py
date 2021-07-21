88db9d5c447e7b9eba6a7170263125688e12a6a0
Angus Whitehead <angusjfw@gmail.com>
2021-07-21 17:47:17 +0100

    Add more unit tests

--- tests/unit/test_process_handler.py
+++ tests/unit/test_process_handler.py
@@ -4,6 +4,8 @@ import logging
 import sys
 import asyncio
 from mock import AsyncMock
+from aiofiles.threadpool.binary import AsyncFileIO
+from asyncio.subprocess import Process
 
 from src.process_handler import ProcessHandler
 
@@ -15,7 +17,7 @@ logging.basicConfig(
 
 
 @pytest.mark.asyncio
-async def test_process_handler():
+async def test_basic():
     p = ProcessHandler(os.environ.get('USER'))
 
     p.on_start = AsyncMock()
@@ -26,6 +28,7 @@ async def test_process_handler():
     # await p.start("echo 'hello\nworld'")
 
     await p.start("bash -c \"echo 'hello\nworld'\"")
+    assert type(p.process) == Process
     p.on_start.assert_called()
 
     await p.process.wait()
@@ -33,4 +36,51 @@ async def test_process_handler():
     await asyncio.sleep(0.2)
 
     p.on_output.assert_called_with('stdout', 'hello\nworld\n')
+    p.on_stop.assert_called_with(0)
+
+
+@pytest.mark.asyncio
+async def test_input():
+    p = ProcessHandler(os.environ.get('USER'))
+
+    p.on_start = AsyncMock()
+    p.on_stop = AsyncMock()
+    p.on_output = AsyncMock()
+
+    await p.start("python3 -c \"print(input())\"")
+    p.on_start.assert_called()
+
+    await p.send_input("hello\n")
+
+    await p.process.wait()
+    # takes some time to complete - there's a 0.1 sleep in there
+    await asyncio.sleep(0.2)
+
+    p.on_output.assert_called_with('stdout', 'hello\n')
+    p.on_stop.assert_called_with(0)
+
+
+@pytest.mark.asyncio
+async def test_pty():
+    p = ProcessHandler(os.environ.get('USER'), pty=True)
+
+    p.on_start = AsyncMock()
+    p.on_stop = AsyncMock()
+    p.on_output = AsyncMock()
+
+    await p.start("python3 -c \"print(input())\"")
+    assert type(p.process) == Process
+    assert p.pty
+    assert type(p.pty_master) == AsyncFileIO
+    assert type(p.pty_slave) == AsyncFileIO
+
+    p.on_start.assert_called()
+
+    await p.send_input("hello\n")
+
+    await p.process.wait()
+    # takes some time to complete - there's a 0.1 sleep in there
+    await asyncio.sleep(0.2)
+
+    p.on_output.assert_called_with('stdout', 'hello\r\nhello\r\n')
     p.on_stop.assert_called_with(0)
