e943b17d822f70a735cbbc5881912ef6dee5d81e
Angus Whitehead <angusjfw@gmail.com>
2021-07-21 17:27:48 +0100

    Add loggers fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,3 +14,17 @@ def create_working_directory():
     os.makedirs(WORKING_DIRECTORY, exist_ok=True)
     yield
     rmtree(WORKING_DIRECTORY)
+
+
+@pytest.fixture(autouse=True)
+def clear_loggers():
+    # this probably isn't the best solution to this problem
+    # https://github.com/pytest-dev/pytest/issues/5502#issuecomment-647157873
+    """Remove handlers from all loggers"""
+    import logging
+    loggers = [logging.getLogger()] + list(
+        logging.Logger.manager.loggerDict.values())
+    for logger in loggers:
+        handlers = getattr(logger, 'handlers', [])
+        for handler in handlers:
+            logger.removeHandler(handler)
