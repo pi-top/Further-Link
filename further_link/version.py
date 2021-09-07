038ed019f4548e0d3c67c2429dbbfe728f13bb0c
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-09-07 18:40:30 +0100

    Use version.txt

--- further_link/version.py
+++ further_link/version.py
@@ -1,1 +1,12 @@
-__version__ = "4.6.0"
+import os
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+__version__ = "N/A"
+try:
+    with open(f"{HERE}/version.txt", "r+") as f:
+        # Reading form a file
+        __version__ = f.read().strip()
+
+except Exception:
+    pass
