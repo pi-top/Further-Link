0a1b7c94ace607ab09243b001d2978fa3a62ecb4
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-09-08 14:19:57 +0100

    No exceptions parsing version.txt

--- further_link/version.py
+++ further_link/version.py
@@ -1,12 +1,6 @@
-import os
+from os import path
 
-HERE = os.path.abspath(os.path.dirname(__file__))
+HERE = path.abspath(path.dirname(__file__))
 
-__version__ = "N/A"
-try:
-    with open(f"{HERE}/version.txt", "r+") as f:
-        # Reading form a file
-        __version__ = f.read().strip()
-
-except Exception:
-    pass
+with open(f"{HERE}/version.txt", "r+") as f:
+    __version__ = f.read().strip()
