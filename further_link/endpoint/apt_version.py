f1b880ff45e6efebb06bfc7da5febef6e9d9187b
Jorge Capona <jcapona@gmail.com>
2022-01-24 17:02:55 -0300

    Use SDK command runner instead of check_output

--- further_link/endpoint/apt_version.py
+++ further_link/endpoint/apt_version.py
@@ -1,9 +1,8 @@
 import json
 import re
-from shlex import split
-from subprocess import CalledProcessError, check_output
 
 from aiohttp import web
+from pitop.common.command_runner import run_command
 
 
 async def apt_version(request):
@@ -13,9 +12,9 @@ async def apt_version(request):
 
 def apt_cache_installed(pkg_name):
     try:
-        command = split(f"apt-cache policy {pkg_name}")
-        output = check_output(command).decode("utf-8")
+        command = f"apt-cache policy {pkg_name}"
+        output = run_command(command, timeout=5)
         match = re.search("Installed: (.*)", output)
         return match.group(1) if match else None
-    except (CalledProcessError, FileNotFoundError):
+    except Exception:
         return None
