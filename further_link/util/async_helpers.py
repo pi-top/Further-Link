33cfbed12b4c37c6b301e8626af5a54423e96b7b
Angus Whitehead <angusjfw@gmail.com>
2022-06-13 18:46:53 +0100

    Fix ringbuf read sometimes dropping final output due to race condition

--- further_link/util/async_helpers.py
+++ further_link/util/async_helpers.py
@@ -45,8 +45,10 @@ async def ringbuf_read(
     # default limit ~ 50 * 256b / 0.1s (128k characters per second)
 
     ringbuf = deque(maxlen=max_chunks)
+    completed = False
 
     async def read():
+        nonlocal completed
         while True:
             read_data = asyncio.create_task(stream_read(stream, chunk_size))
             wait_done = asyncio.create_task(done_condition())
@@ -54,22 +56,25 @@ async def ringbuf_read(
             done = await race([read_data, wait_done])
 
             if read_data not in done:
+                completed = True
                 break
 
             result = read_data.result()
+
             if result == b"":
+                completed = True
                 break
 
             ringbuf.append(read_data.result())
 
     async def write():
+        nonlocal completed
         while True:
             # let data buffer in ringbuf for buffer_time or until process ends
             # if process ends ringbuf still needs to be handled
-            done = False
             try:
                 await asyncio.wait_for(done_condition(), timeout=buffer_time)
-                done = True
+                completed = True
             except (TimeoutError, asyncio.TimeoutError):
                 pass
 
@@ -79,7 +84,11 @@ async def ringbuf_read(
                 output = data.decode(encoding="utf-8")
                 if output_callback:
                     await output_callback(output)
-            if done:
+            if completed:
                 break
 
-    await race([asyncio.create_task(read()), asyncio.create_task(write())])
+    # when read is completed it will set a flag causing write to complete after
+    # flushing a final time
+    return await asyncio.wait(
+        [asyncio.create_task(read()), asyncio.create_task(write())]
+    )
