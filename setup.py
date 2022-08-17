f75e0b8d1d9bba69758ee70b6d1b258ee84ae4e4
Jorge Capona <jcapona@gmail.com>
2022-08-17 15:58:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,5 @@ import setuptools
 
 if __name__ == "__main__":
     setuptools.setup(
-        scripts=["scripts/further-link-vnc"],
-        version=environ.get("PYTHON_PACKAGE_VERSION", "0.0.1.dev1").replace('"', ""),
+        version=environ.get("PYTHON_PACKAGE_VERSION", "0.0.1.dev1").replace('"', "")
     )
