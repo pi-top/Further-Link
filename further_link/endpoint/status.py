c103dfa98954bc6dc489e8e043e52d50b78b8e4a
Angus Whitehead <angusjfw@gmail.com>
2022-11-04 13:52:06 +0000

    Update live check

--- further_link/endpoint/status.py
+++ further_link/endpoint/status.py
@@ -22,10 +22,15 @@ async def live(_):
     if not id_generator.has_ids():
         raise Exception("All ids are in use.")
 
+    async_task = asyncio.create_task(asyncio.sleep(0.1))
+    done = await timeout(async_task, 0.2)
+    if async_task not in done:
+        raise Exception("Async coroutines blocked.")
+
     sleep_thread = asyncio.to_thread(partial(sleep, 0.1))
     async_thread_task = asyncio.create_task(sleep_thread)
     done = await timeout(async_thread_task, 0.2)
     if async_thread_task not in done:
-        raise Exception("No available async thread executors.")
+        raise Exception("Async threads blocked.")
 
     return web.Response(text="OK")
