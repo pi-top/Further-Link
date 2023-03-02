2f53d91edbaa5aaf842ac6f4f6e32c737af91f15
Jorge Capona <jcapona@gmail.com>
2023-03-02 16:46:59 -0300

    Revert "Upload files to projects folder"
    This reverts commit 29cefe1fb14d5bbfd5aea38a3419a7555487264b.

--- further_link/endpoint/upload.py
+++ further_link/endpoint/upload.py
@@ -1,17 +1,9 @@
 import json
-from os import path
 
 from aiohttp import web
 
 from ..util.upload import BadUpload, directory_is_valid, do_upload
-from ..util.user_config import default_user, get_working_directory
-
-
-def is_miniscreen_project(directory):
-    for filename in directory.get("files", {}):
-        if filename.endswith(".cfg"):
-            return True
-    return False
+from ..util.user_config import get_working_directory
 
 
 async def upload(request):
@@ -26,16 +18,7 @@ async def upload(request):
         if not directory_is_valid(directory):
             raise web.HTTPBadRequest()
 
-        # Upload to further directory
         await do_upload(directory, work_dir, user)
-
-        # Upload to projects directory to run using miniscreen app
-        if is_miniscreen_project(directory):
-            if user is None:
-                user = default_user()
-            user_home = path.expanduser(f"~{user}")
-            project_work_dir = f"{user_home}/Desktop/Projects"
-            await do_upload(directory, project_work_dir, user, use_cache=False)
 
     except (web.HTTPBadRequest, json.decoder.JSONDecodeError):
         raise web.HTTPBadRequest()
