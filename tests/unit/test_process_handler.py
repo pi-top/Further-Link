2145910c37f4dfb84409c3956ff477ce10415858
Jorge Capona <jcapona@gmail.com>
2023-04-28 18:25:09 -0400

    Send screenshot only on display activity

--- tests/unit/test_process_handler.py
+++ tests/unit/test_process_handler.py
@@ -93,7 +93,7 @@ async def test_pty():
 
 
 @pytest.mark.asyncio
-async def test_novnc():
+async def test_novnc_with_display_activity():
     p = ProcessHandler(user)
 
     p.on_start = AsyncMock()
@@ -118,7 +118,7 @@ pause()
 
         vnc_start.assert_called_with(
             display_id=p.id,
-            on_display_activity=p.on_display_activity,
+            on_display_activity=p.handle_display_activity,
             ssl_certificate=VNC_CERTIFICATE_PATH,
             with_window_manager=True,
             height=novncOptions.get("height"),
@@ -131,6 +131,10 @@ pause()
     await asyncio.sleep(0.2)
     p.on_output.assert_called_with("stdout", "doing fake graphics!\n")
 
+    # Mock display activity
+    await p.handle_display_activity(100)
+    p.on_display_activity.assert_called_with(100)
+
     with patch(
         "further_link.runner.process_handler.async_stop", AsyncMock()
     ) as vnc_stop:
@@ -140,6 +144,58 @@ pause()
 
         vnc_stop.assert_called_with(p.id)
 
+    # On exit, sends video output
+    assert p.on_output.call_count == 2
     p.on_output.assert_called_with("video", jpeg_pixel_b64)
+
+    p.on_stop.assert_called_with(-15)
+
+
+@pytest.mark.asyncio
+async def test_novnc_no_display_activity():
+    p = ProcessHandler(user)
+
+    p.on_start = AsyncMock()
+    p.on_stop = AsyncMock()
+    p.on_output = AsyncMock()
+    p.on_display_activity = AsyncMock()
+
+    code = """\
+from signal import pause
+print('no graphics in here!')
+pause()
+"""
+    novncOptions = {"enabled": True, "height": 620, "width": 780}
+
+    with patch(
+        "further_link.runner.process_handler.async_start", AsyncMock()
+    ) as vnc_start:
+        await p.start(f'python3 -u -c "{code}"', novncOptions=novncOptions)
+
+        vnc_start.assert_called_with(
+            display_id=p.id,
+            on_display_activity=p.handle_display_activity,
+            ssl_certificate=VNC_CERTIFICATE_PATH,
+            with_window_manager=True,
+            height=novncOptions.get("height"),
+            width=novncOptions.get("width"),
+            screenshot_timeout=1,
+        )
+
+    p.on_start.assert_called()
+
+    await asyncio.sleep(0.2)
+    p.on_output.assert_called_with("stdout", "no graphics in here!\n")
+
+    with patch(
+        "further_link.runner.process_handler.async_stop", AsyncMock()
+    ) as vnc_stop:
+        await p.stop()
+        await p.process.wait()
+        await asyncio.sleep(0.2)
+
+        vnc_stop.assert_called_with(p.id)
+
+    assert p.on_output.call_count == 1
 
     p.on_stop.assert_called_with(-15)
